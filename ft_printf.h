3fe0ce6f957b07c48f2b07217018c30165b63553
Bruno Aguiar <aguiarsilva@gmail.com>
2023-12-11 13:51:51 +0100

    add files .c .h

---
+++ ft_printf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baguiar- <baguiar-@student.42wolfsburg.de  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/11 13:36:32 by baguiar-          #+#    #+#             */
+/*   Updated: 2023/12/11 13:42:48 by baguiar-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+#define FT_PRINTF_H
+
+#include "libft/libft.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+int ft_printf(const char *format, ...);
+
+#endif
+
