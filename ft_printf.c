98e8cd5e69549caa6ea21e91e229404e09686757
Bruno Aguiar <aguiarsilva@gmail.com>
2023-12-20 07:48:53 +0100

    updated ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -6,18 +6,26 @@
 /*   By: baguiar- <baguiar-@student.42wolfsburg.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 13:36:52 by baguiar-          #+#    #+#             */
-/*   Updated: 2023/12/19 23:56:47 by baguiar-         ###   ########.fr       */
+/*   Updated: 2023/12/20 07:48:32 by baguiar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int ft_fmt(const char **form, va_list chg, int i)
+static int ft_fmt(char fmt, va_list ap)
 {
   int out_len;
 
   out_len = 0;
-
+  if (fmt == 'c')
+      out_len += ft_putchar_c(va_arg(ap, int));
+  else if (fmt == 's')
+      out_len += ft_putstr_s(va_arg(ap, char *));
+  else if (fmt == 'd')
+      out_len += ft_putint_d_i((long)va_arg(ap, int));
+  else if (fmt == 'x')
+      out_len += 1; 
+  return (out_len);
 }
 
 int ft_printf(const char *format, ...)
